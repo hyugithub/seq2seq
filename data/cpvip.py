1ec6b1c04af3012a5d7a744bdff96e856dbe1c40
Ubuntu <ubuntu@ip-172-31-35-102.ec2.internal>
2017-12-26 14:00:25 +0000

    working but refused connection error happens

--- data/cpvip.py
+++ data/cpvip.py
@@ -2,24 +2,49 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 import numpy as np
 import time
+import sys
+
+ts = time.time()
+list_journals = []
+with open("/data/papers/paper.txt", 'r', encoding='utf-8') as f:
+    f2 = f.read()
+    list_journals = f2.split("\n")    
+    print(len(list_journals))
+    print("done")
+
+#sys.exit()
+
+start = 0
+step = 10
+end = start+step
 
 class MySpider(scrapy.Spider):
     name = "quotes"
 
+    def __init__(self):
+        self.start = 0
+        self.end = 10
+        self.step = 10
+
     def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
+        urls = list_journals
+        self.start += self.step
+        self.end += self.step
+        count = 0
         for url in urls:
+            count += 1
+            if count > 0 and count % 2000 == 0:
+                time.sleep(np.random.uniform()*10+10)
             yield scrapy.Request(url=url, callback=self.parse)
 
-        with open("../../site/cpvip.log", 'a') as logfile:
+        with open("/data/log/cpvip.log", 'a') as logfile:
             logfile.write(urls[-1]+"\n")
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = '../../site/quotes-%s.html' % page
+        page = response.url
+        page = page.replace("http://lib.cqvip.com/qk/","")
+        page = page.replace("/","_")
+        filename = '/data/papers/%s' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
 
@@ -29,9 +54,7 @@ process = CrawlerProcess({ \
 	'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)' \
     })
 
-for param in np.arange(3):
-    process.crawl(MySpider)
-    time.sleep(np.random.uniform()*10 + 10)
+process.crawl(MySpider)
 
 process.start()
 
