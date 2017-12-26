5a140bfd2916d6e9926c5e3c4d9f42a59d2691ae
Ubuntu <ubuntu@ip-172-31-56-157.ec2.internal>
2017-12-26 15:01:53 +0000

    adding list of files already processed

--- data/cpvip.py
+++ data/cpvip.py
@@ -6,13 +6,21 @@ import sys
 
 ts = time.time()
 list_journals = []
-with open("/data/papers/paper.txt", 'r', encoding='utf-8') as f:
+with open("/data/paper.txt", 'r', encoding='utf-8') as f:
     f2 = f.read()
     list_journals = f2.split("\n")    
-    print(len(list_journals))
-    print("done")
+    print("raw entry: ", len(list_journals))
 
-#sys.exit()
+list_done = []
+with open("/data/done.txt", "r") as done:
+    f3 = done.read()
+    list_done = f3.split("\n")
+list_done = [p.split("_")[-1] for p in list_done]
+list_done = set(list_done)
+print("entries done: ", len(list_done))
+
+list_journals = [u for u in list_journals if (u.split("/")[-1] in list_done) == False ]
+print("after removing: ", len(list_journals))
 
 start = 0
 step = 10
@@ -25,16 +33,13 @@ class MySpider(scrapy.Spider):
         self.start = 0
         self.end = 10
         self.step = 10
+        self.count = 0
 
     def start_requests(self):
         urls = list_journals
         self.start += self.step
         self.end += self.step
-        count = 0
         for url in urls:
-            count += 1
-            if count > 0 and count % 2000 == 0:
-                time.sleep(np.random.uniform()*10+10)
             yield scrapy.Request(url=url, callback=self.parse)
 
         with open("/data/log/cpvip.log", 'a') as logfile:
@@ -49,6 +54,10 @@ class MySpider(scrapy.Spider):
             f.write(response.body)
 
         self.log('Saved file %s' % filename)
+        self.count += 1
+        if self.count > 0 and self.count % 500 == 0:
+            time.sleep(np.random.uniform()*10+10)
+        time.sleep(np.random.uniform()*0.25+0.2)
 
 process = CrawlerProcess({ \
 	'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)' \
